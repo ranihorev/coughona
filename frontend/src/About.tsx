a5a1ac14e32c70ece6ab1430169655ff2ac39f4b
Rani <ranihorev@gmail.com>
2020-03-25 19:05:40 -0700

    updated about

--- frontend/src/About.tsx
+++ frontend/src/About.tsx
@@ -5,66 +5,67 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Link } from '@material-ui/core';
 
 export const About: React.FC = () => {
-    return (
-      <div
-        css={{
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '100%',
-          fontFamily: 'Roboto, sans-serif',
-        }}
-      >
-        <div css={{ maxWidth: 800, padding: 30, overflow: 'auto', fontSize: 16, textAlign: 'left', lineHeight: 1.5 }}>
-          <p>
-            Coughona was created with the vision that simple, quick and home-based risk assessments of COVID-19 should
-            be available to all. Since right now there aren't enough COVID-19 test kits for the masses, we are exploring
-            an alternative way to test for COVID-19.
-          </p>
-          <p>
-            <Link href="https://www.scihive.org/collection/5e7be8b6debc5169f3bceb2e/" target="_blank">
-              Studies
-            </Link>{' '}
-            showed that AI, specifically Deep Learning, is very effective at identifying various sort of lung related
-            diseases like the flu, bronchiolitis and pneumonia. There is a good chance the same holds true for COVID-19.
-          </p>
-          <p>
-            Our vision is to collect the biggest database of COVID-19 coughs together with a related questionnaire, in
-            order to build the first AI algorithm that can predict COVID-19 from the comfort of your home.
-          </p>
-          <p>
-            We are a group of entrepreneurs and researchers that specialize in healthcare and technology, and are hoping
-            that this tool can make a small difference in addressing this global epidemic.
-          </p>
-          <p>
-            For more questions or comments, feel free to contact us at{' '}
-            <Link href="mailto:info@coughona.com" target="_blank">
-              info@coughona.com
-            </Link>
-            .
-          </p>
-          <p>
-            Wishing you all good health, <br />
-            <Link href="https://www.linkedin.com/in/yaronhadad/" target="_blank">
-              Yaron
-            </Link>
-            ,{' '}
-            <Link href="https://www.linkedin.com/in/lautman/" target="_blank">
-              Ziv
-            </Link>
-            , Dvir, Idan, Tyler and{' '}
-            <Link href="https://www.linkedin.com/in/rani-horev/" target="_blank">
-              Rani
-            </Link>
-          </p>
-          <div>
-            <Link component={RouterLink} to="/">
-              Back to Survey
-            </Link>
-          </div>
+  return (
+    <div
+      css={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        fontFamily: 'Roboto, sans-serif',
+      }}
+    >
+      <div css={{ maxWidth: 800, padding: 30, overflow: 'auto', fontSize: 16, textAlign: 'left', lineHeight: 1.5 }}>
+        <p>
+          Coughona was created with the vision that simple, quick, and at-home risk assessments of COVID-19 should be
+          available to all. Since right now there aren't enough COVID-19 test kits for the masses, we are exploring an
+          alternative way to assess risk for COVID-19.
+        </p>
+        <p>
+          Recent{' '}
+          <Link href="https://www.scihive.org/collection/5e7be8b6debc5169f3bceb2e/" target="_blank">
+            studies
+          </Link>{' '}
+          have shown that deep learning can identify lung diseases including flu, bronchiolitis and pneumonia. We
+          believe the same may hold true for COVID-19.
+        </p>
+        <p>
+          Our vision is to collect the biggest database of COVID-19 coughs and symptoms, allowing us to build an AI
+          algorithm to predict COVID-19 risk from the comfort of your home.
+        </p>
+        <p>
+          We are a group of entrepreneurs and researchers that specialize in healthcare and technology, and are hoping
+          that this tool can make a small difference in addressing this global epidemic.
+        </p>
+        <p>
+          For more questions or comments, feel free to contact us at{' '}
+          <Link href="mailto:info@coughona.com" target="_blank">
+            info@coughona.com
+          </Link>
+          .
+        </p>
+        <p>
+          Wishing you all good health, <br />
+          <Link href="https://www.linkedin.com/in/yaronhadad/" target="_blank">
+            Yaron
+          </Link>
+          ,{' '}
+          <Link href="https://www.linkedin.com/in/lautman/" target="_blank">
+            Ziv
+          </Link>
+          , Dvir, Idan, Tyler and{' '}
+          <Link href="https://www.linkedin.com/in/rani-horev/" target="_blank">
+            Rani
+          </Link>
+        </p>
+        <div>
+          <Link component={RouterLink} to="/">
+            Back to Survey
+          </Link>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
